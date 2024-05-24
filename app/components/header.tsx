be055f30447604fe9ae3bac93ec8ec9c25cfaaa1
jotta2021 <joanderson2572@gmail.com>
2024-05-23 22:30:06 -0300

    check

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,75 +1,61 @@
-"use client"
-import { Button } from "@/components/ui/button"
-import Image from "@/node_modules/next/image"
-import { Switch } from "@/components/ui/switch"
+'use client'
 
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Switch } from "@/components/ui/switch";
 
 interface LanguageProps {
-  language: 'pt-br' | 'en',
-    setLanguage: (language: string) => void;
-    darkMode: string,
-    setDarkMode: (darkMode: boolean) => void;
-    scroll: () => void;
-
+  language: 'pt-br' | 'en';
+  setLanguage: (language: 'pt-br' | 'en') => void;
+  darkMode: boolean;
+  setDarkMode: (darkMode: boolean) => void;
+  scroll: (sectionId: string) => void;
 }
 
-
-
 export default function Header({ language, setLanguage, darkMode, setDarkMode, scroll }: LanguageProps) {
-    const changeLanguage = () => {
-        const newLanguage = language === 'pt-br' ? 'en' : 'pt-br'; //alterna entre os idiomas
-        setLanguage(newLanguage)
+  const changeLanguage = () => {
+    const newLanguage = language === 'pt-br' ? 'en' : 'pt-br'; //alterna entre os idiomas
+    setLanguage(newLanguage);
+  };
+
+  const texts = {
+    'pt-br': {
+      about: 'Sobre mim',
+      projects: 'Projetos',
+      contact: 'Contato',
+      Habilidades: 'Habilidades'
+    },
+    'en': {
+      about: 'About me',
+      projects: 'Projects',
+      contact: 'Contact',
+      Habilidades: 'Skills'
     }
-    const texts = {
-        'pt-br': {
-            about: 'Sobre mim',
-            projects: 'Projetos',
-            contact: 'Contato',
-            Habilidades: 'Hablidades'
-        },
-        'en': {
-            about: 'About me',
-            projects: 'Projects',
-            contact: 'Contact',
-            Habilidades: 'Skills'
-        }
-    }
-    const { about, projects, contact,Habilidades } = texts[language]
-    console.log('darkMode', darkMode)
-    return (
-        <div className={"flex w-[100vw] fixed top-0 z-50"} style={{ backgroundColor: darkMode ? 'black' : 'white' }}>
-            <div className="flex justify-between my-3 mx-5 w-full items-center">
+  };
 
-                <span className={darkMode ? 'text-white  max-sm:hidden' : ' text-muted-foreground  max-sm:hidden'}>Joanderson Luan <span className="text-cyan-500">{'</>'}</span></span>
-                <ul className="flex gap-4 items-center">
-                    <li className='cursor-pointer hover:text-cyan-500'
-                        onClick={() => scroll('about')}
-                    >{about}</li>
-                    <li className='cursor-pointer  hover:text-cyan-500'
-                        onClick={() => scroll('projects')}
-                    >{projects}</li>
-                    <li className='cursor-pointer  hover:text-cyan-500'
-                        onClick={() => scroll('skills')}
-                    >{Habilidades}</li>
-
-                    <Switch
-                        checked={darkMode}
-                        onCheckedChange={setDarkMode}
-                    />
-
-
-
-                    <Button variant='outline' size='icon'
-                        onClick={changeLanguage}
-                    >
-                        <Image src={language === 'pt-br' ? '/square.png' : '/eua.png'} width={30} height={30}
-                            alt='language'
-                        />
-                    </Button>
-
-                </ul>
-
-            </div>
-        </div >
-    )
+  const { about, projects, contact, Habilidades } = texts[language];
+  
+  return (
+    <div className={"flex w-[100vw] fixed top-0 z-50"} style={{ backgroundColor: darkMode ? 'black' : 'white' }}>
+      <div className="flex justify-between my-3 mx-5 w-full items-center">
+        <span className={darkMode ? 'text-white max-sm:hidden' : 'text-muted-foreground max-sm:hidden'}>
+          Joanderson Luan <span className="text-cyan-500">{'</>'}</span>
+        </span>
+        <ul className="flex gap-4 items-center">
+          <li className='cursor-pointer hover:text-cyan-500' onClick={() => scroll('about')}>{about}</li>
+          <li className='cursor-pointer hover:text-cyan-500' onClick={() => scroll('projects')}>{projects}</li>
+          <li className='cursor-pointer hover:text-cyan-500' onClick={() => scroll('skills')}>{Habilidades}</li>
+          
+          <Switch
+            checked={darkMode}
+            onCheckedChange={setDarkMode}
+          />
+          
+          <Button variant='outline' size='icon' onClick={changeLanguage}>
+            <Image src={language === 'pt-br' ? '/square.png' : '/eua.png'} width={30} height={30} alt='language' />
+          </Button>
+        </ul>
+      </div>
+    </div>
+  );
 }
